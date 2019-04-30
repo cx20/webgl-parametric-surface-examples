131ebf9a630f86ce79c8587c97663ba2e591aa24
cx20 <y-shimano@hondarer-soft.com>
2019-04-30 23:56:12 +0900

    Added  lightgl.js example

---
+++ examples/lightgl/roman-surface/index.js
@@ -0,0 +1,49 @@
+var angle = 0;
+var gl = GL.create();
+
+gl.onupdate = function(seconds) {
+    angle += 45 * seconds;
+};
+
+gl.ondraw = function() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.loadIdentity();
+    gl.translate(0, 0, -2);
+    gl.rotate(-60, 1, 0, 0);
+    gl.rotate(angle, 0, 0, 1);
+
+    // Prepare coordinate data of Roman surface
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    gl.begin(gl.LINE_STRIP);
+    var ustep = Math.PI * 10 / 180;
+    var vstep = Math.PI * 10 / 180;
+    for (var v = 0; v <= 2 * Math.PI; v += vstep) {
+        for (var u = 0; u <= 2 * Math.PI; u += ustep) {
+            RomanSurface(u, v);
+            RomanSurface(u + ustep, v);
+            RomanSurface(u + ustep, v + vstep);
+            RomanSurface(u, v + vstep);
+        }
+    }
+    gl.end();
+};
+
+function RomanSurface(u, v) {
+    var x = Math.sin(u) * Math.cos(u) * Math.sin(v) * Math.sin(v);
+    var y = Math.sin(u) * Math.sin(v) * Math.cos(v);
+    var z = Math.cos(u) * Math.sin(v) * Math.cos(v);
+    gl.color(x + 0.5, y + 0.5, z + 0.5, 1.0);
+    gl.vertex(x, y, z);
+}
+
+gl.fullscreen();
+gl.animate();
---
+++ examples/lightgl/seashell-surface/index.js
@@ -0,0 +1,48 @@
+
+var angle = 0;
+var gl = GL.create();
+
+gl.onupdate = function(seconds) {
+    angle += 45 * seconds;
+};
+
+gl.ondraw = function() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.loadIdentity();
+    gl.translate(0, 0, -2);
+    gl.rotate(60, 1, 0, 0);
+    gl.rotate(angle, 0, 0, -1);
+
+    // Prepare coordinate data of seashell surface
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    gl.begin(gl.POINTS);
+    gl.pointSize(2.0);
+    var ustep = Math.PI * 5 / 180;
+    var vstep = Math.PI * 5 / 180;
+    var a = 0.2;
+    var b = 0.6;
+    var c = 0.2;
+    var n = 2.0;
+    for (var v = 0; v <= 2 * Math.PI; v += vstep) {
+        for (var u = 0; u <= 2 * Math.PI; u += ustep) {
+            x = (a * (1 - v / (2 * Math.PI)) * (1 + Math.cos(u)) + c) * Math.cos(n * v);
+            y = (a * (1 - v / (2 * Math.PI)) * (1 + Math.cos(u)) + c) * Math.sin(n * v);
+            z = b * v / (2 * Math.PI) + a * (1 - v / (2 * Math.PI)) * Math.sin(u);
+            gl.color(x + 0.5, y + 0.5, z + 0.5, 1.0);
+            gl.vertex(x, y, z);
+        }
+    }
+    gl.end();
+};
+
+gl.fullscreen();
+gl.animate();
