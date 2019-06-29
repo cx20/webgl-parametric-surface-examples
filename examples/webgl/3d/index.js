0c4efa9ddc74164f7be3c0ab12beaa2dc535e391
cx20 <y-shimano@hondarer-soft.com>
2019-06-29 18:53:22 +0900

    Added WebGL example

---
+++ examples/webgl/3d/index.js
@@ -0,0 +1,145 @@
+let c, gl;
+let aLoc = [];
+let uLoc = [];
+
+let positions = [];
+let colors = [];
+
+let translation;
+let scale;
+let eye;
+let center;
+let up;
+let view;
+let mvMatrix = mat4.create();
+let pMatrix = mat4.create();
+let vertexBuffer;
+let colorBuffer;
+
+let rad = 0;
+let theta = 0;
+
+function initWebGL() {
+    c = document.getElementById("c");
+    gl = c.getContext("experimental-webgl");
+
+    resizeCanvas();
+    window.addEventListener("resize", function(){
+        resizeCanvas();
+    });
+}
+
+function resizeCanvas() {
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+    gl.viewport(0, 0, c.width, c.height);
+}
+
+function initShaders() {
+    let p = gl.createProgram();
+    let vs = gl.createShader(gl.VERTEX_SHADER);
+    let fs = gl.createShader(gl.FRAGMENT_SHADER);
+    let v = document.getElementById("vs").textContent;
+    let f = document.getElementById("fs").textContent;
+    gl.shaderSource(vs, v);
+    gl.shaderSource(fs, f);
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+    gl.attachShader(p, vs);
+    gl.attachShader(p, fs);
+    gl.linkProgram(p);
+    gl.useProgram(p);
+    aLoc[0] = gl.getAttribLocation(p, "position");
+    aLoc[1] = gl.getAttribLocation(p, "color");
+    gl.enableVertexAttribArray(aLoc[0]);
+    gl.enableVertexAttribArray(aLoc[1]);
+    uLoc[0] = gl.getUniformLocation(p, "pjMatrix");
+    uLoc[1] = gl.getUniformLocation(p, "mvMatrix");
+}
+
+function initBuffers() {
+    // 3次元関数の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    for (let j = -10; j < 10; j += 0.2) {
+        for (let i = -10; i < 10; i += 0.2) {
+            let x = i;
+            let y = Math.sin(Math.sqrt(i*i+j*j))/Math.sqrt(i*i+j*j);
+            let z = j;
+            positions = positions.concat([x/10, y/5, z/10]);
+            colors = colors.concat([x/10+0.5, y+0.5, z/10+0.5, 1.0]);
+        }
+    }
+
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.DYNAMIC_DRAW);
+    gl.vertexAttribPointer(aLoc[0], 3, gl.FLOAT, false, 0, 0);
+
+    colorBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
+    gl.vertexAttribPointer(aLoc[1], 3, gl.FLOAT, false, 0, 0);
+}
+
+function render(){
+    rad += Math.PI * 1.0 / 180.0;
+
+    mat4.perspective(pMatrix, 45, window.innerWidth / window.innerHeight, 0.1, 1000.0);
+    mat4.identity(mvMatrix);
+    var translation = vec3.create();
+    vec3.set(translation, 0.0, -1.0, -3.0);
+    mat4.translate(mvMatrix, mvMatrix, translation);
+    mat4.rotate(mvMatrix, mvMatrix, rad, [0, 1, 0]);
+
+    gl.uniformMatrix4fv(uLoc[0], false, pMatrix);
+    gl.uniformMatrix4fv(uLoc[1], false, mvMatrix);
+    
+    draw();
+
+    requestAnimationFrame(render);
+}
+
+function draw() {
+    theta += Math.PI * 1/180;
+    
+    let k = 0;
+    for (let j = -10; j < 10; j += 0.2) {
+        for (let i = -10; i < 10; i += 0.2) {
+            let x = i;
+            let y = Math.sin(Math.sqrt(i * i + j * j) + theta) / Math.sqrt(i * i + j * j);
+            let z = j;
+            let x2 = x / 10;
+            let y2 = y / 2;
+            let z2 = z / 10;
+            positions[k * 3 + 0] = x2;
+            positions[k * 3 + 1] = y2;
+            positions[k * 3 + 2] = z2;
+            colors[k * 3 + 0] = x2 + 0.5;
+            colors[k * 3 + 1] = y2 + 0.5;
+            colors[k * 3 + 2] = z2 + 0.5;
+            k++;
+        }
+    }
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(positions));
+    
+    //gl.drawArrays(gl.LINE_STRIP, 0, positions.length / 3);
+    gl.drawArrays(gl.POINTS, 0, positions.length / 3);
+    gl.flush();
+}
+
+window.onload = function() {
+    initWebGL();
+    initShaders();
+    initBuffers();
+    render();
+};
