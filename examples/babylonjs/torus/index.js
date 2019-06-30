f515a624b199171c4c6d5b90c04ed9588ec8e319
cx20 <y-shimano@hondarer-soft.com>
2019-06-30 17:30:17 +0900

    Added Babylon.js example

---
+++ examples/babylonjs/torus/index.js
@@ -0,0 +1,74 @@
+﻿let createScene = function (engine) {
+    let scene = new BABYLON.Scene(engine);
+    let camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0.0, -2.5), scene);
+    scene.clearColor = new BABYLON.Color3(0, 0, 0);
+
+    let positions = [];
+    let colors = [];
+    let indices = [];
+    let idx = 0;
+    let theta = 0.0;
+
+    // Prepare torus coordinate data
+     
+    // x = (R + r cos(v)) cos(u)
+    // y = (R + r cos(v)) sin(u)
+    // z = r sin(v)
+    // 
+    // u is an element of the set of numbers [0, 2 pi]
+    // v is an element of the set of numbers [0, 2 pi]
+    
+    let ustep = Math.PI * 5 / 180;
+    let vstep = Math.PI * 5 / 180;
+    let R = 0.5;
+    let r = 0.2;
+    for (let v = 0; v <= 2 * Math.PI; v += vstep) {
+        for (let u = 0; u <= 2 * Math.PI; u += ustep) {
+            let x = (R + r * Math.cos(v)) * Math.cos(u);
+            let y = (R + r * Math.cos(v)) * Math.sin(u);
+            let z = r * Math.sin(v);
+            positions.push(x, y, z);
+            colors.push(x + 0.5, y + 0.5, z + 0.5, 1);
+            indices.push(idx++);
+        }
+    }
+    
+    let mesh = new BABYLON.Mesh("mesh", scene);
+    let vertexData = new BABYLON.VertexData();
+    
+    vertexData.positions = positions;
+    vertexData.indices = indices;
+    vertexData.colors = colors;
+    vertexData.applyToMesh(mesh, true);
+
+    let material = new BABYLON.ShaderMaterial("material", scene, {
+        vertexElement: "vs",
+        fragmentElement: "fs",
+    }, {
+        attributes: ["position", "color"],
+        uniforms: ["worldViewProjection"]
+    });
+    //material.fillMode = BABYLON.Material.LineStripDrawMode;
+    material.fillMode = BABYLON.Material.PointFillMode;
+    mesh.material = material;
+    mesh.material.backFaceCulling = false;
+    
+    scene.registerBeforeRender(function () {
+        //mesh.rotation.x = theta;
+        //mesh.rotation.y = theta;
+        //mesh.rotation.z = theta;
+        mesh.rotate(BABYLON.Axis.X, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+        mesh.rotate(BABYLON.Axis.Y, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+        //mesh.rotate(BABYLON.Axis.Z, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+    });
+    
+    return scene;
+};
+
+let canvas = document.querySelector("#renderCanvas");
+let engine = new BABYLON.Engine(canvas, true);
+let scene = createScene( engine );
+
+engine.runRenderLoop(function () {
+    scene.render();
+});
