f642d453583aa1623e6e78fad6f9ae29f4e7c76e
cx20 <y-shimano@hondarer-soft.com>
2019-06-30 17:09:36 +0900

    Added Babylon.js example

---
+++ examples/babylonjs/slime/index.js
@@ -0,0 +1,129 @@
+﻿let createScene = function (engine) {
+    let scene = new BABYLON.Scene(engine);
+    let camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0.0, -2.5), scene);
+    scene.clearColor = new BABYLON.Color3(0, 0, 0);
+
+    let positions = [];
+    let colors = [];
+    let indices = [];
+    let idx = 0;
+    let theta = 0.0;
+/*
+    // Prepare coordinate data of apple surface
+
+    // x = cos (u) (4 + 3.8 cos (v))
+    // y = sin (u) (4 + 3.8 cos (v))
+    // z = (cos (v) + sin (v) -1) (1 + sin (v)) log (1-pi v / 10) +7.5 sin (v)
+    // 
+    // u is an element of the set of numbers [0, 2 pi]
+    // v is an element of the set of numbers [-pi, pi]
+
+    let ustep = Math.PI * 5 / 180;
+    let vstep = Math.PI * 5 / 180;
+    for (let v = -Math.PI; v <= Math.PI; v += vstep) {
+        for (let u = 0; u <= 2 * Math.PI; u += ustep) {
+            let x = Math.cos(u) * (4 + 3.8 * Math.cos(v));
+            let y = Math.sin(u) * (4 + 3.8 * Math.cos(v));
+            let z = (Math.cos(v)+Math.sin(v)-1) * (1+Math.sin(v)) * Math.log(1-Math.PI * v/10)+7.5 * Math.sin(v);
+            let x2 = x/20;
+            let y2 = y/20;
+            let z2 = z/20;
+            positions.push(x2, z2, y2);
+            colors.push(x2 + 0.5, y2 + 0.5, z2 + 0.5, 1);
+            indices.push(idx++);
+        }
+    }
+*/
+    // Prepare slime coordinate data
+    // x = b (cos(v) (r + sin(v))) cos(u)
+    // y = a (r + sin(v))
+    // z = b (cos(v) (r + sin(v))) sin(u)
+    let a = 1;
+    let b = 1;
+    let c = 1;
+    let r = 1;
+    let ustep = Math.PI * 5 / 180;
+    let vstep = Math.PI * 5 / 180;
+    for (let v = -Math.PI/2; v <= Math.PI/2; v += vstep) {
+        for (let u = 0; u <= 2 * Math.PI; u += ustep) {
+            let x = b * (Math.cos(v) * (r + Math.sin(v))) * Math.cos(u);
+            let y = a * (r + Math.sin(v));
+            let z = b * (Math.cos(v) * (r + Math.sin(v))) * Math.sin(u);
+            let x2 = x/2;
+            let y2 = y/2 - 0.5;
+            let z2 = z/2;
+            positions.push(x2, z2, y2);
+            colors.push(x2 + 0.5, y2 + 0.5, z2 + 0.5, 1);
+            indices.push(idx++);
+        }
+    }
+
+    
+    let mesh = new BABYLON.Mesh("mesh", scene);
+    let vertexData = new BABYLON.VertexData();
+    
+    vertexData.positions = positions;
+    vertexData.indices = indices;
+    vertexData.colors = colors;
+    vertexData.applyToMesh(mesh, true);
+
+    let material = new BABYLON.ShaderMaterial("material", scene, {
+        vertexElement: "vs",
+        fragmentElement: "fs",
+    }, {
+        attributes: ["position", "color"],
+        uniforms: ["worldViewProjection"]
+    });
+    //material.fillMode = BABYLON.Material.LineStripDrawMode;
+    material.fillMode = BABYLON.Material.PointFillMode;
+    mesh.material = material;
+    mesh.material.backFaceCulling = false;
+    
+    scene.registerBeforeRender(function () {
+        draw();
+        //mesh.rotation.x = theta;
+        //mesh.rotation.y = theta;
+        //mesh.rotation.z = theta;
+        mesh.rotate(BABYLON.Axis.X, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+        mesh.rotate(BABYLON.Axis.Y, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+        mesh.rotate(BABYLON.Axis.Z, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+    });
+    
+    function draw() {
+        let positions = mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind);
+        let colors = mesh.getVerticesData(BABYLON.VertexBuffer.ColorKind);
+        theta += Math.PI * 1/180;
+        let k = 0;
+        for (let j = -10; j < 10; j += 0.2) {
+            for (let i = -10; i < 10; i += 0.2) {
+                let x = i;
+                let y = Math.sin(Math.sqrt(i * i + j * j) + theta) / Math.sqrt(i * i + j * j);
+                let z = j;
+                let x2 = x / 10;
+                let y2 = y / 2;
+                let z2 = z / 10;
+                positions[k * 3 + 0] = x2;
+                positions[k * 3 + 1] = y2;
+                positions[k * 3 + 2] = z2;
+                colors[k * 4 + 0] = x2 + 0.5;
+                colors[k * 4 + 1] = y2 + 0.5;
+                colors[k * 4 + 2] = z2 + 0.5;
+                colors[k * 4 + 3] = 1.0;
+                k++;
+            }
+        }
+
+        mesh.updateVerticesData(BABYLON.VertexBuffer.PositionKind, positions);
+        mesh.updateVerticesData(BABYLON.VertexBuffer.ColorKind, colors);
+    }
+    
+    return scene;
+};
+
+let canvas = document.querySelector("#renderCanvas");
+let engine = new BABYLON.Engine(canvas, true);
+let scene = createScene( engine );
+
+engine.runRenderLoop(function () {
+    scene.render();
+});
