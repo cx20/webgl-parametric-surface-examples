48154a5eb7168ac500256a12c08576285de4a0ff
cx20 <y-shimano@hondarer-soft.com>
2019-06-30 17:17:02 +0900

    Added Babylon.js example

---
+++ examples/babylonjs/heart/index.js
@@ -0,0 +1,98 @@
+﻿let createScene = function (engine) {
+    let scene = new BABYLON.Scene(engine);
+    let camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0.0, -2.5), scene);
+    scene.clearColor = new BABYLON.Color3(0, 0, 0);
+
+    let positions = [];
+    let colors = [];
+    let indices = [];
+    let idx = 0;
+    let theta = 0.0;
+
+    // Prepare coordinate data of heart surface
+    let num = 16;
+    for (let i = -Math.PI * num; i <= Math.PI * num; i++) {
+        for (let j = -num; j <= num; j++) {
+            let theta = i / num;
+            let z0 = j / num;
+            let r = 4 * Math.sqrt(1 - z0 * z0) * Math.pow(Math.sin(Math.abs(theta)), Math.abs(theta));
+            let x1 = r * Math.sin(theta);
+            let y1 = r * Math.cos(theta);
+            let z1 = z0;
+            let x2 = x1 / 8;
+            let y2 = y1 / 8;
+            let z2 = z1 / 8;
+            positions.push(x2, z2, y2);
+            colors.push(x2 + 0.5, y2 + 0.5, z2 + 0.5, 1);
+            indices.push(idx++);
+        }
+    }
+    
+    let mesh = new BABYLON.Mesh("mesh", scene);
+    let vertexData = new BABYLON.VertexData();
+    
+    vertexData.positions = positions;
+    vertexData.indices = indices;
+    vertexData.colors = colors;
+    vertexData.applyToMesh(mesh, true);
+
+    let material = new BABYLON.ShaderMaterial("material", scene, {
+        vertexElement: "vs",
+        fragmentElement: "fs",
+    }, {
+        attributes: ["position", "color"],
+        uniforms: ["worldViewProjection"]
+    });
+    //material.fillMode = BABYLON.Material.LineStripDrawMode;
+    material.fillMode = BABYLON.Material.PointFillMode;
+    mesh.material = material;
+    mesh.material.backFaceCulling = false;
+    
+    scene.registerBeforeRender(function () {
+        draw();
+        //mesh.rotation.x = theta;
+        //mesh.rotation.y = theta;
+        //mesh.rotation.z = theta;
+        mesh.rotate(BABYLON.Axis.X, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+        mesh.rotate(BABYLON.Axis.Y, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+        mesh.rotate(BABYLON.Axis.Z, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+    });
+    
+    function draw() {
+        let positions = mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind);
+        let colors = mesh.getVerticesData(BABYLON.VertexBuffer.ColorKind);
+        theta += Math.PI * 1/180;
+        let k = 0;
+        for (let j = -10; j < 10; j += 0.2) {
+            for (let i = -10; i < 10; i += 0.2) {
+                let x = i;
+                let y = Math.sin(Math.sqrt(i * i + j * j) + theta) / Math.sqrt(i * i + j * j);
+                let z = j;
+                let x2 = x / 10;
+                let y2 = y / 2;
+                let z2 = z / 10;
+                positions[k * 3 + 0] = x2;
+                positions[k * 3 + 1] = y2;
+                positions[k * 3 + 2] = z2;
+                colors[k * 4 + 0] = x2 + 0.5;
+                colors[k * 4 + 1] = y2 + 0.5;
+                colors[k * 4 + 2] = z2 + 0.5;
+                colors[k * 4 + 3] = 1.0;
+                k++;
+            }
+        }
+
+        mesh.updateVerticesData(BABYLON.VertexBuffer.PositionKind, positions);
+        mesh.updateVerticesData(BABYLON.VertexBuffer.ColorKind, colors);
+    }
+    
+    return scene;
+};
+
+let canvas = document.querySelector("#renderCanvas");
+let engine = new BABYLON.Engine(canvas, true);
+let scene = createScene( engine );
+
+engine.runRenderLoop(function () {
+    scene.render();
+});
