880ae92a5579775802b0729abf8a045c7c4b8488
cx20 <y-shimano@hondarer-soft.com>
2019-06-30 17:21:52 +0900

    Update Babylon.js example

--- examples/babylonjs/slime/index.js
+++ examples/babylonjs/slime/index.js
@@ -8,32 +8,7 @@
     let indices = [];
     let idx = 0;
     let theta = 0.0;
-/*
-    // Prepare coordinate data of apple surface
 
-    // x = cos (u) (4 + 3.8 cos (v))
-    // y = sin (u) (4 + 3.8 cos (v))
-    // z = (cos (v) + sin (v) -1) (1 + sin (v)) log (1-pi v / 10) +7.5 sin (v)
-    // 
-    // u is an element of the set of numbers [0, 2 pi]
-    // v is an element of the set of numbers [-pi, pi]
-
-    let ustep = Math.PI * 5 / 180;
-    let vstep = Math.PI * 5 / 180;
-    for (let v = -Math.PI; v <= Math.PI; v += vstep) {
-        for (let u = 0; u <= 2 * Math.PI; u += ustep) {
-            let x = Math.cos(u) * (4 + 3.8 * Math.cos(v));
-            let y = Math.sin(u) * (4 + 3.8 * Math.cos(v));
-            let z = (Math.cos(v)+Math.sin(v)-1) * (1+Math.sin(v)) * Math.log(1-Math.PI * v/10)+7.5 * Math.sin(v);
-            let x2 = x/20;
-            let y2 = y/20;
-            let z2 = z/20;
-            positions.push(x2, z2, y2);
-            colors.push(x2 + 0.5, y2 + 0.5, z2 + 0.5, 1);
-            indices.push(idx++);
-        }
-    }
-*/
     // Prepare slime coordinate data
     // x = b (cos(v) (r + sin(v))) cos(u)
     // y = a (r + sin(v))
